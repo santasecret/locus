aacfd6aa55f8bc92a89fe35103666396d47dd7e5
Ameya Shenoy <shenoy.ameya@gmail.com>
2020-07-27 00:38:09 +0530

    feat: add ansible to setup k3s
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- infra/setup.tf
+++ infra/setup.tf
@@ -1,5 +1,6 @@
 
 variable "hcloud_token" {}
+variable "ssh_keys" {}
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
@@ -12,6 +13,32 @@ resource "hcloud_server" "locus-n1" {
   location = "hel1"
   image = "ubuntu-20.04"
   server_type = "cpx11"
-  ssh_keys = ["Predator"]
+  ssh_keys = var.ssh_keys
+
+  provisioner "local-exec" {
+    command = "echo ${hcloud_server.locus-n1.ipv4_address}"
+  }
+}
+
+resource "null_resource" "install_k3s" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook ansible/playbooks/k3s.yml -u root -i ${hcloud_server.locus-n1.ipv4_address}, -v" # TODO add sleep 60 in the end just for safety
+  }
+}
+
+resource "null_resource" "install_jenkins" {
+  depends_on = [null_resource.install_k3s]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${hcloud_server.locus-n1.ipv4_address}"
+  }
 }
 

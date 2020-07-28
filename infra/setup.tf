95f533f581ea9c504ba75d61e2f932d155a79d02
Ameya Shenoy <shenoy.ameya@gmail.com>
2020-07-28 23:00:20 +0530

    fix: minor cleanup
    Signed-off-by: Ameya Shenoy <shenoy.ameya@gmail.com>

--- infra/setup.tf
+++ infra/setup.tf
@@ -26,7 +26,7 @@ resource "null_resource" "setup_k3s" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 60 && cd ansible && ansible-playbook playbooks/k3s.yml -u root -i ${hcloud_server.locus-n1.ipv4_address}, -v && cd ../cert-manager && sh install_cert_manager.sh && kubectl apply -f issuer.yaml"
+    command = "sleep 60 && cd ansible && ansible-playbook playbooks/k3s.yml -u root -i ${hcloud_server.locus-n1.ipv4_address}, -v && cd ../cert-manager && sh install_cert_manager.sh && sleep 60 && kubectl apply -f issuer.yaml"
   }
 }
 
